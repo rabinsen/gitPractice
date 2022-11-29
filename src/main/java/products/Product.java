489b56feecbd604f3a77b2bdd8db7226d8930124
Rabin Senchuri <rivansen007@gmail.com>
2022-11-28 20:25:06 -0500

    Product domain updated

--- src/main/java/products/Product.java
+++ src/main/java/products/Product.java
@@ -4,17 +4,27 @@ public class Product {
 	private String productNumber;
 	private String productName;
 	private String productCategory;
+	private String maker;
 	private int stock;
 
 
 	public Product() {
 	}
 
-	public Product(String productNumber, String productName, String productCategory, int stock) {
+	public Product(String productNumber, String productName, String productCategory, String maker, int stock) {
 		this.productNumber = productNumber;
 		this.productName = productName;
 		this.productCategory = productCategory;
+		this.maker = maker;
 		this.stock = stock;
+	}
+
+	public String getMaker() {
+		return maker;
+	}
+
+	public void setMaker(String maker) {
+		this.maker = maker;
 	}
 
 	public String getProductCategory() {
@@ -47,5 +57,16 @@ public class Product {
 
 	public void setStock(int stock) {
 		this.stock = stock;
+	}
+
+	@Override
+	public String toString() {
+		return "Product{" +
+				"productNumber='" + productNumber + '\'' +
+				", productName='" + productName + '\'' +
+				", productCategory='" + productCategory + '\'' +
+				", maker='" + maker + '\'' +
+				", stock=" + stock +
+				'}';
 	}
 }
